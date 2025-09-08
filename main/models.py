8d94b7b15dbc6426f60148a9b60cf37284598499
alfino157 <alfino.feriza@gmail.com>
2025-09-09 00:08:33 +0700

    Menambahkan model product pada main app, routing urls.py, dan membuat fungsi views.py

---
+++ main/models.py
@@ -0,0 +1,33 @@
+import uuid
+from django.db import models
+
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('Shorts', 'shorts'),
+        ('Ball', 'ball'),
+        ('Shoes', 'shoes'),
+        ('Shirts', 'shirts'),
+        ('Hoodies', 'hoodies'),
+        ('Socks', 'socks'),
+    ]
+
+    #name price desc thumb cat isfield
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    thumbnail = models.URLField(blank=True, null=True)
+    price = models.PositiveIntegerField(default=0)
+    stock = models.PositiveBigIntegerField(default=0)
+    is_featured = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
+    
+    # @property
+    # def is_news_hot(self):
+    #     return self.news_views > 20
+        
+    # def increment_views(self):
+    #     self.news_views += 1
+    #     self.save()
